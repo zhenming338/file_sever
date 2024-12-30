03645a8fd425a58653db98c257a4f1681213deac
river <lichangjiang0490@qq.com>
2024-12-30 14:20:29 +0800

    git init

---
+++ src/test/java/org/river/file_server/FileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.river.file_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
